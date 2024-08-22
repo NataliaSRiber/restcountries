9dcfa1445eb7ada50c8ff8632f0c1b7ec87ad3b3
Natalia <natalia.ribeiro@globalfinanceiro.com.br>
2024-08-22 16:33:59 -0300

    cria o country service

---
+++ src/app/country.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+  private baseUrl = 'https://restcountries.com/v3.1';
+  constructor( private http: HttpClient) { }
+
+  getCountries(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/all`).pipe(
+      // função de comparação de strings para determinar a ordem de duas strings
+      map((countries: any[]) => countries.sort((a, b) => a.name.official.localeCompare(b.name.official))
+      )
+    );
+  }
+
+  getCountryByName(name: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/name/${name}`).pipe(map(data => data[0] ))
+  }
+  
+}
