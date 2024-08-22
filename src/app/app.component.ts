0f209db1c40fa69d4fa6c3dd9a3c4103f85ea835
Natalia <natalia.ribeiro@globalfinanceiro.com.br>
2024-08-22 16:32:23 -0300

    adiciona arquivos app

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  // onPageChange(page: number) {
+  //   this.p = page; // Atualiza a página atual com o número da nova página
+  // }
+}
