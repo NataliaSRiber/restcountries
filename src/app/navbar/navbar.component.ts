fc6d46a16356dc03015f63552a4c88cb9bce9c97
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-08-23 16:38:44 -0300

    adiciona os outputs dos selects e atualiza a função onSearch

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,25 +9,35 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  @Output() search = new EventEmitter<string>(); // evento para emitir pesquisa o componente pai
+  @Output() search = new EventEmitter<string>();
+  @Output() continentChange = new EventEmitter<string>();
+  @Output() languageChange = new EventEmitter<string>();
+
   input = new FormControl<string>('');
-  searchService: any;
+  selectedContinent: string = '';
+  selectedLanguage: string = '';
 
   constructor(){}
 
-    // onSearch() {;
-    //   const inputValue = this.input.value;
-    //     if(inputValue) {
-    //       this.searchService.search(inputValue);
-    //   }
-    // }
-  // modo transmitindo diretamente para o pai e com o navbar somente na home  e nao no app.component
-  onSearch() {;
-    const inputValue = this.input.value;
-      if(inputValue) {
-      this.search.emit(inputValue);
+  onSearch() {
+    if (this.selectedContinent) {
+      this.continentChange.emit(this.selectedContinent);
+    } else if (this.selectedLanguage) {
+      this.languageChange.emit(this.selectedLanguage);
+    } else if (this.input.value) {
+      this.search.emit(this.input.value!);
+    } else {
+      this.search.emit
     }
   }
-}
 
-// Para deixar a navbar no appcomponent as logicas evem ficar la, so que nao, obg
+  onContinentChange(event: any) {
+    this.selectedContinent = event.target.value;
+    this.continentChange.emit(this.selectedContinent);
+  }
+
+  onLanguageChange(event: any) {
+    this.selectedLanguage = event.target.value;
+    this.languageChange.emit(this.selectedLanguage);
+  }
+}
