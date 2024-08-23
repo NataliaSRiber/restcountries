bc683cf21b9a0d7b756a38cb31997faa4df9f01c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-08-23 16:45:38 -0300

    atualiza a navbar para a nova função

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,10 +9,7 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  @Output() search = new EventEmitter<string>();
-  @Output() continentChange = new EventEmitter<string>();
-  @Output() languageChange = new EventEmitter<string>();
-
+  @Output() search = new EventEmitter<{ input: string; continent: string; language: string }>();
   input = new FormControl<string>('');
   selectedContinent: string = '';
   selectedLanguage: string = '';
@@ -20,24 +17,18 @@ export class NavbarComponent {
   constructor(){}
 
   onSearch() {
-    if (this.selectedContinent) {
-      this.continentChange.emit(this.selectedContinent);
-    } else if (this.selectedLanguage) {
-      this.languageChange.emit(this.selectedLanguage);
-    } else if (this.input.value) {
-      this.search.emit(this.input.value!);
-    } else {
-      this.search.emit
-    }
+    this.search.emit({
+      input: this.input.value || '',
+      continent: this.selectedContinent,
+      language: this.selectedLanguage
+    });
   }
 
   onContinentChange(event: any) {
     this.selectedContinent = event.target.value;
-    this.continentChange.emit(this.selectedContinent);
   }
 
   onLanguageChange(event: any) {
     this.selectedLanguage = event.target.value;
-    this.languageChange.emit(this.selectedLanguage);
   }
 }
