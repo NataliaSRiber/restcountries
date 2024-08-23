965022ee4be37456cf25c2582876f0be4997aca8
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-08-23 16:47:31 -0300

    volta a função anterior

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CardsListComponent } from '../cards-list/cards-list.component';
 import { CountryService } from '../country.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +13,22 @@ import { CountryService } from '../country.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+
+  input = new FormControl('');
+  selectedContinent: string = '';
+  selectedLanguage: string = '';
   collection: any[] = [];
   isLoading = true;
+  // p: number = 1;
+  // itemsPerPage: number = 10;
 
-  constructor(
-    private countryService: CountryService,
-  ) {}
-
+  constructor(private countryService: CountryService) { }
+    
   ngOnInit(): void {
-    this.loadAllCountries();
+    this.loadCountries();
   }
   
-  loadAllCountries(): void {
+  loadCountries(): void {
     this.isLoading = true;
     this.countryService.getCountries().subscribe({
       next: (data) => {
@@ -31,37 +36,65 @@ export class HomeComponent implements OnInit {
         this.isLoading = false;
       },
       error: (err) => {
-        console.error('Error loading countries', err);
+        console.error('Error searching countries', err);
         this.isLoading = false;
       }
-    });
+    })
   }
 
-  onSearch(filters: { input: string; continent: string; language: string }) {
+  onSearch(inputValue: string) {
     this.isLoading = true;
-    let searchObservable;
-
-    if (filters.input) {
-      searchObservable = this.countryService.getCountryByName(filters.input);
-    } else if (filters.continent) {
-      searchObservable = this.countryService.getFilteredCountriesByRegion(filters.continent);
-    } else if (filters.language) {
-      searchObservable = this.countryService.getFilteredCountriesByLanguage(filters.language);
-    } else {
-      this.loadAllCountries();
-      return;
-    }
-
-    searchObservable.subscribe({
+    this.countryService.getCountryByName(inputValue).subscribe({
       next: (data) => {
         this.collection = Array.isArray(data) ? data : [data];
         this.isLoading = false;
       },
       error: (err) => {
-        console.error('Error searching countries', err);
-        this.collection = [];
+        console.error('Error fetching country by name', err);
         this.isLoading = false;
       }
     });
   }
+  
+  onContinentSearch(continent: string) {
+    if(continent) {
+      this.isLoading = true;
+      this.countryService.getFilteredCountriesByRegion(continent).subscribe({
+        next: (data) => {
+          console.log(data)
+          this.collection = Array.isArray(data) ? data : [data];
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Error fetching countries by continent', err);
+          this.isLoading = false;
+        }
+      });
+    } else {
+      this.isLoading = true;
+      this.loadCountries();
+    }
+  }
+  
+  onLanguageSearch(language: string) {
+    if (language) {
+      this.isLoading = true;
+      this.countryService.getFilteredCountriesByLanguage(language).subscribe({
+        next: (data) => {
+          this.collection = Array.isArray(data) ? data : [data];
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Error fetching countries by language', err);
+          this.isLoading = false;
+        }
+      });
+    } else {
+      this.isLoading = true;
+      this.loadCountries();
+    }
+  }
+  
+  
+  
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,7 +9,10 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  @Output() search = new EventEmitter<{ input: string; continent: string; language: string }>();
+  @Output() search = new EventEmitter<string>();
+  @Output() continentChange = new EventEmitter<string>();
+  @Output() languageChange = new EventEmitter<string>();
+
   input = new FormControl<string>('');
   selectedContinent: string = '';
   selectedLanguage: string = '';
@@ -17,18 +20,24 @@ export class NavbarComponent {
   constructor(){}
 
   onSearch() {
-    this.search.emit({
-      input: this.input.value || '',
-      continent: this.selectedContinent,
-      language: this.selectedLanguage
-    });
+    if (this.selectedContinent) {
+      this.continentChange.emit(this.selectedContinent);
+    } else if (this.selectedLanguage) {
+      this.languageChange.emit(this.selectedLanguage);
+    } else if (this.input.value) {
+      this.search.emit(this.input.value!);
+    } else {
+      this.search.emit
+    }
   }
 
   onContinentChange(event: any) {
     this.selectedContinent = event.target.value;
+    this.continentChange.emit(this.selectedContinent);
   }
 
   onLanguageChange(event: any) {
     this.selectedLanguage = event.target.value;
+    this.languageChange.emit(this.selectedLanguage);
   }
 }
